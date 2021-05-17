10ebbcc19d025e1a8f022312e8213a17c1f84840
betogroo <luizhumberto@gmail.com>
2021-05-17 20:54:25 -0300

    feat: create initial app-bar

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,45 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
+    <v-app-bar
+      app
+      color="primary"
+      height="100"
+      class="rounded-t-xl justify-content-beetween"
+    >
+      <v-toolbar-title>
         <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+          width="180"
+          src="https://trello-attachments.s3.amazonaws.com/60a29fb49a600b4353b63879/60a29fb49a600b4353b63898/x/c91b80ab499f30151e362f75bd531dbf/image.png"
+        >
+        </v-img>
+      </v-toolbar-title>
+     <v-spacer class="d-sm-none d-md-flex"></v-spacer>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-text-field
+      class="d-none d-sm-flex"
+        label="Busque por algo"
+        solo-inverted
+        dark
+        hide-details
+        full-width
+      ></v-text-field>
+      <v-spacer class="d-sm-none d-md-flex"></v-spacer>
+     
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn class="d-flex d-sm-none" icon dark>
+        <v-icon size="32">mdi-magnify</v-icon>
       </v-btn>
+
+      <v-btn class="d-md-none d-xl-flex" icon dark>
+        <v-icon size="32">mdi-menu</v-icon>
+      </v-btn>
+
+      <div class="d-none d-md-flex align-center justify-beetween">
+        <v-avatar size="38">
+          <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" />
+        </v-avatar>
+        <span class="white--text pl-3">Harry</span>
+      </div>
     </v-app-bar>
 
     <v-main>
@@ -41,10 +50,15 @@
 
 <script>
 export default {
-  name: "App",
+  name: 'App',
 
   data: () => ({
     //
-  }),
-};
+  })
+}
 </script>
+<style scoped>
+.v-text-field {
+  border-radius: 8px;
+}
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,16 @@
-import Vue from "vue";
-import Vuetify from "vuetify/lib/framework";
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib/framework'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
-export default new Vuetify({});
+const options = {
+  theme: {
+    themes: {
+      light: {
+        primary: '#051D3B'
+      }
+    }
+  }
+}
+
+export default new Vuetify(options)
