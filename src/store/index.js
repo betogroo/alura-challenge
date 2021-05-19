8fb3807ed759947c943f8f25bbe39f659f014255
betogroo <luizhumberto@gmail.com>
2021-05-19 20:23:23 -0300

    feat: create changeBordeColor actions and state

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,18 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    editorBorderColor: '#6BD1FF'
+  },
+  mutations: {
+    CHANGE_EDITOR_BORDER_COLOR(state, payload) {
+      state.editorBorderColor = payload
+    }
+  },
+  actions: {
+    changeEditorBorderColor({ commit }, payload) {
+      commit('CHANGE_EDITOR_BORDER_COLOR', payload)
+    }
+  },
   modules: {}
 })
