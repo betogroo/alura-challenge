0d26b7fd23b7845e91420aac5d0b37e70623607e
betogroo <luizhumberto@gmail.com>
2021-05-20 14:35:35 -0300

    refactor: change psq settings

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  transpileDependencies: ['vuetify']
+  transpileDependencies: ['vuetify'],
+
+  pwa: {
+    name: 'Alura DEV',
+    themeColor: '#051D3B',
+    msTileColor: '#051D3B',
+    manifestOptions: {
+      background_color: '#051D3B'
+    }
+  }
 }
