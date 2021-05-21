74a3a643a95d6a824124112b8a97bd716ed77749
betogroo <luizhumberto@gmail.com>
2021-05-20 21:09:39 -0300

    refactor: change Editor Component data to props

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <v-card
-      :color="editor.color"
+      :color="color"
       outlined
       dark
       class="ma-1"
-      :style="`border: 24px solid ${editorBorderColor}`"
+      :style="`border: 24px solid ${borderColor}`"
     >
       <v-card-title class="pa-0 pt-2 pl-4">
         <v-icon
-          v-for="macBall in editor.macBalls"
+          v-for="macBall in macBalls"
           :key="macBall"
           x-small
           left
@@ -19,46 +19,34 @@
         </v-icon>
       </v-card-title>
       <v-card-text class="pa-0 pl-1">
-        <v-textarea
-          :background-color="editor.color"
-          auto-grow
-          solo
-          :value="script"
-        >
+        <v-textarea :background-color="color" auto-grow solo :value="text">
         </v-textarea>
       </v-card-text>
     </v-card>
-    <div class="mt-5 mx-1">
-      <v-btn block dark color="#133055">Visualizar com o highlight</v-btn>
-    </div>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
 export default {
   name: 'Editor',
 
-  data: () => ({
-    editor: {
-      color: '#000000',
-      macBalls: ['#FF5F56', '#FFBD2E', '#27C93F']
+  props: {
+    color: {
+      type: String,
+      default: '#000000'
     },
-    script: `const pluckDeep = key => obj => key.split('.').reduce((accum, key) => accum[key], obj)
-const compose = (...fns) => res => fns.reduce((accum, next) => next(accum), res)
-const unfold = (f, seed) => {
-  const go = (f, seed, acc) => {
-    const res = f(seed)
-    return res ? go(f, res[1], acc.concat([res[0]])) : acc
-  }
-  return go(f, seed, [])
-}`
-  }),
+    text: {
+      type: String,
+      default: ''
+    },
+    borderColor: {
+      type: String,
+      default: '#6BD1FF'
+    }
+  },
 
-  computed: {
-    ...mapState(['editorBorderColor'])
-  }
+  data: () => ({
+    macBalls: ['#FF5F56', '#FFBD2E', '#27C93F']
+  })
 }
 </script>
-
-<style></style>
--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -2,7 +2,10 @@
   <v-container class="pt-0">
     <v-row>
       <v-col cols="12" lg="8">
-        <editor />
+        <editor :borderColor="editorBorderColor" :text="script" />
+        <div class="mt-5 mx-1">
+          <v-btn block dark color="#133055">Visualizar com o highlight</v-btn>
+        </div>
       </v-col>
       <v-col>
         <form-project />
@@ -12,6 +15,7 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import Editor from '@/components/Editor.vue'
 import FormProject from '@/components/FormProject.vue'
 export default {
@@ -25,7 +29,20 @@ export default {
   data: () => ({
     form: {
       color: '#6BD1FF'
-    }
-  })
+    },
+    script: `const pluckDeep = key => obj => key.split('.').reduce((accum, key) => accum[key], obj)
+const compose = (...fns) => res => fns.reduce((accum, next) => next(accum), res)
+const unfold = (f, seed) => {
+  const go = (f, seed, acc) => {
+    const res = f(seed)
+    return res ? go(f, res[1], acc.concat([res[0]])) : acc
+  }
+  return go(f, seed, [])
+}`
+  }),
+
+  computed: {
+    ...mapState(['editorBorderColor'])
+  }
 }
 </script>
