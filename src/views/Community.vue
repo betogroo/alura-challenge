25bdabb3233a6a3d09409d97f24a0ce70efc09df
betogroo <luizhumberto@gmail.com>
2021-05-21 00:33:03 -0300

    feat: Community View - in progress

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -1,10 +1,53 @@
 <template>
-  <h1>Comunidade</h1>
+  <v-container>
+    <v-row v-for="(row, i) in slicedProjects" :key="i">
+      <v-col
+        v-for="(project, i) in row"
+        :key="`c${i}`"
+        cols="12"
+        :lg="row.length === 2 ? '6' : '12'"
+      >
+        <editor :borderColor="project.borderColor" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import Editor from '@/components/Editor.vue'
 export default {
-  name: 'ViewCommunity'
+  name: 'ViewCommunity',
+
+  components: {
+    Editor
+  },
+
+  data: () => ({
+    projects: [
+      { borderColor: '#7A8B23' },
+      { borderColor: '#61238B' },
+      { borderColor: '#8B2A23' },
+      { borderColor: '#238B59' },
+      { borderColor: '#FFFFFF' },
+      { borderColor: '#BABACA' }
+    ]
+  }),
+
+  methods: {
+    sliceProjects(rows) {
+      let slicedProjects = []
+      for (let i = 0; i < this.projects.length; i = i + rows) {
+        slicedProjects.push(this.projects.slice(i, i + rows))
+      }
+      return slicedProjects
+    }
+  },
+
+  computed: {
+    slicedProjects() {
+      return this.sliceProjects(2)
+    }
+  }
 }
 </script>
 
