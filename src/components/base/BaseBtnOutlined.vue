d12037ddaace0a1aee8df44e1fcd27bc21620f72
betogroo <luizhumberto@gmail.com>
2021-05-24 19:32:18 -0300

    feat: create base-btn-outilined - in progress

---
+++ src/components/base/BaseBtnOutlined.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-hover>
+    <v-btn
+      class="white--text"
+      light
+      :block="block"
+      :color="color"
+      v-ripple="false"
+    >
+      <slot>{{ text }}</slot>
+    </v-btn>
+  </v-hover>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+
+  props: {
+    text: {
+      type: String,
+      default: 'Submit'
+    },
+    block: {
+      type: Boolean,
+      default: false
+    },
+    color: {
+      type: String,
+      default: 'rgba(80, 129, 251, 0.08)'
+    },
+    dark: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
