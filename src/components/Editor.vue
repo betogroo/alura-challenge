dcc5febbce90f8577df4ea2388784d40ad1e78a4
betogroo <luizhumberto@gmail.com>
2021-06-07 16:33:20 -0300

    feat : create buttom to print code and show in dialog

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,64 +1,97 @@
 <template>
   <div>
-    <v-card
-      outlined
-      dark
-      flat
-      :to="!editable ? { name: 'Project', params: { id: project.id } } : ''"
-      class="ma-1 mb-0"
-      :style="`border: 24px solid ${project.borderColor || '#FFFFFF'}`"
-      :height="size"
-    >
-      <v-card-title class="d-flex justify-space-between pa-0 pt-2 px-4">
-        <div>
-          <v-icon
-            v-for="macBall in macBalls"
-            :key="macBall"
-            x-small
-            left
-            :color="macBall"
-          >
-            mdi-circle
-          </v-icon>
-        </div>
-        <div>
-          <v-icon v-if="project.language" large
-            >mdi-language-{{ project.language.icon }}</v-icon
-          >
-        </div>
-      </v-card-title>
-      <v-card-text class="pa-0 pl-1">
-        <template v-if="!isHighlight">
-          <v-textarea
-            v-model="form.script"
-            hide-details
-            flat
-            solo
-            no-resize
-            background-color="#1d262f"
-            @input="updateValue"
-          >
-          </v-textarea>
-        </template>
-        <template v-else>
-          <div v-highlight>
-            <pre
-              :class="`language-${
-                project.language ? project.language.code : ''
-              }`"
+    <div ref="printMe">
+      <v-card
+        outlined
+        dark
+        flat
+        :to="!editable ? { name: 'Project', params: { id: project.id } } : ''"
+        class="ma-1 mb-0"
+        :style="`border: 24px solid ${project.borderColor || '#FFFFFF'}`"
+        :height="size"
+      >
+        <v-card-title class="d-flex justify-space-between pa-0 pt-2 px-4">
+          <div>
+            <v-icon
+              v-for="macBall in macBalls"
+              :key="macBall"
+              x-small
+              left
+              :color="macBall"
             >
-                <code> 
-                  {{ `\n${form.script}` }}
-                </code>
-              </pre>
+              mdi-circle
+            </v-icon>
           </div>
-        </template>
-      </v-card-text>
-    </v-card>
+          <div>
+            <v-icon v-if="project.language" large
+              >mdi-language-{{ project.language.icon }}</v-icon
+            >
+          </div>
+        </v-card-title>
+        <v-card-text class="pa-0 pl-1">
+          <div v-if="!isHighlight">
+            <v-textarea
+              v-model="form.script"
+              hide-details
+              flat
+              solo
+              no-resize
+              background-color="#1d262f"
+              @input="updateValue"
+            >
+            </v-textarea>
+          </div>
+          <div v-else>
+            <div v-highlight>
+              <pre
+                :class="`language-${
+                  project.language ? project.language.code : ''
+                }`"
+              >
+                  <code> 
+                    {{ `\n${form.script}` }}
+                  </code>
+                </pre>
+            </div>
+          </div>
+        </v-card-text>
+      </v-card>
+    </div>
     <div v-if="editable" class="mt-5 mx-1">
-      <base-btn-outlined dark block type="button" @click="toggleHighlight">
-        {{ !isHighlight ? 'Visualizar com o Highlight' : 'Editar Código' }}
-      </base-btn-outlined>
+      <v-row>
+        <v-col :cols="isHighlight ? 10 : 12">
+          <base-btn-outlined dark block type="button" @click="toggleHighlight">
+            {{ !isHighlight ? 'Visualizar com o Highlight' : 'Editar Código' }}
+          </base-btn-outlined>
+        </v-col>
+        <v-col v-if="isHighlight">
+          <v-dialog
+            v-model="dialog"
+            fullscreen
+            hide-overlay
+            transition="dialog-bottom-transition"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <base-btn-icon
+                icon="share"
+                v-bind="attrs"
+                v-on="on"
+                @action="print"
+              />
+            </template>
+            <v-card>
+              <v-toolbar dark color="primary">
+                <base-btn-icon icon="close" @action="dialog = false" />
+                <v-toolbar-title>Compartilhar Código</v-toolbar-title>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-card class="pt-2">
+                <v-img max-width="640px" :src="output" />
+              </v-card>
+            </v-card>
+          </v-dialog>
+        </v-col>
+      </v-row>
     </div>
   </div>
 </template>
@@ -84,7 +117,9 @@ export default {
 
   data: () => ({
     macBalls: ['#FF5F56', '#FFBD2E', '#27C93F'],
-    isHighlight: !false
+    isHighlight: !false,
+    output: null,
+    dialog: false
   }),
 
   computed: {
@@ -99,6 +134,13 @@ export default {
     },
     updateValue(data) {
       this.$emit('input', data)
+    },
+    async print() {
+      const el = this.$refs.printMe
+      const options = {
+        type: 'dataURL'
+      }
+      this.output = await this.$html2canvas(el, options)
     }
   }
 }
